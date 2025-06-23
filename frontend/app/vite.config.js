48bacc9e9df10a09877f288b4bfd2cbc5b605af8
dimastack <lydimdream@gmail.com>
2025-06-24 01:42:18 +0200

    Update vite config and build dir in the Dockerfile

--- frontend/app/vite.config.js
+++ frontend/app/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // ensures relative paths, important for Nginx
+  base: './',
+  build: {
+    // explicitly state the output directory
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5001',
